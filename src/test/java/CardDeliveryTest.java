f2a6ee4a9f292869f74bb022367d8218d9ad9ac0
NetoIQA75 <all-slavin@yandex.ru>
2024-02-08 16:51:37 +0300

    Initial commit

---
+++ src/test/java/CardDeliveryTest.java
@@ -0,0 +1,20 @@
+import com.codeborne.selenide.SelectorMode;
+import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.*;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+
+    @BeforeEach
+    void setup() {
+        open("http://localhost:9999/");
+    }
+
+    @Test
+    public void shouldTestDelivery(){
+
+    }
+}
