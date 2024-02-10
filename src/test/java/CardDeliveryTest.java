7290fcff3a67b2a20f74b79be59bedd7e1c119ed
NetoIQA75 <all-slavin@yandex.ru>
2024-02-10 19:49:42 +0300

    Fixed tests according to the comments

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -1,20 +1,16 @@
-import com.codeborne.selenide.SelectorMode;
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
 import org.junit.jupiter.api.*;
-import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 
-import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.time.LocalDate;
-import java.util.Calendar;
-import java.util.Date;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static java.time.LocalTime.now;
 
 public class CardDeliveryTest {
 
@@ -23,22 +19,26 @@ public class CardDeliveryTest {
         open("http://localhost:9999");
     }
 
+    public String generateDate(int days) { // метод генерации даты плюс n дней
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
+
     @Test
     public void shouldTestFormWithCorrectData() { // проверка Happy Path
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
-
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, 5); // прибавление 5 дней к текущей дате
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(4); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("Васильев Петр");
         form.$("[data-test-id=phone] input").setValue("+79811567894");
         form.$("[data-test-id=agreement]").click();
         form.$$("button").last().click();
-        $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        // $("[data-test-id=notification]").shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + planningDate), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -46,20 +46,18 @@ public class CardDeliveryTest {
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Моосква");
 
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, 5); // прибавление 5 дней к текущей дате
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(4); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("Васильев Петр");
         form.$("[data-test-id=phone] input").setValue("+79811567894");
         form.$("[data-test-id=agreement]").click();
         form.$$("button").last().click();
 
-        $("[data-test-id=city] .input__sub").shouldHave(exactText("Доставка в выбранный город недоступна")); // проверка текста ошибки
-        boolean b = $("[data-test-id=city].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
-        Assertions.assertTrue(b);
+        $("[data-test-id=city].input_invalid")
+                .shouldHave(Condition.text("Доставка в выбранный город недоступна"))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -67,20 +65,18 @@ public class CardDeliveryTest {
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
 
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, -5); // отнимание 5 дней от текущей даты
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(-5); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("Васильев Петр");
         form.$("[data-test-id=phone] input").setValue("+79811567894");
         form.$("[data-test-id=agreement]").click();
         form.$$("button").last().click();
 
-        $("[data-test-id=date] .input__sub").shouldHave(exactText("Заказ на выбранную дату невозможен")); // проверка текста ошибки
-        boolean b = $("[data-test-id=date] .input_invalid").isDisplayed(); // проверка наличия класса input_invalid
-        Assertions.assertTrue(b);
+        $("[data-test-id=date] .input_invalid")
+                .shouldHave(Condition.text("Заказ на выбранную дату невозможен"))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -88,20 +84,18 @@ public class CardDeliveryTest {
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
 
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, 5); // прибавление 5 дней к текущей дате
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(4); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("568# $$@");
         form.$("[data-test-id=phone] input").setValue("+79811567894");
         form.$("[data-test-id=agreement]").click();
         form.$$("button").last().click();
 
-        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.")); // проверка текста ошибки
-        boolean b = $("[data-test-id=name].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
-        Assertions.assertTrue(b);
+        $("[data-test-id=name].input_invalid")
+                .shouldHave(Condition.text("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -109,20 +103,18 @@ public class CardDeliveryTest {
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
 
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, 5); // прибавление 5 дней к текущей дате
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(4); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("Васильев Петр");
         form.$("[data-test-id=phone] input").setValue("+7981sacsa881255s+");
         form.$("[data-test-id=agreement]").click();
         form.$$("button").last().click();
 
-        $("[data-test-id=phone] .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678.")); // проверка текста ошибки
-        boolean b = $("[data-test-id=phone].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
-        Assertions.assertTrue(b);
+        $("[data-test-id=phone].input_invalid")
+                .shouldHave(Condition.text("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."))
+                .shouldBe(Condition.visible);
     }
 
     @Test
@@ -130,17 +122,14 @@ public class CardDeliveryTest {
         SelenideElement form = $("form");
         form.$("[data-test-id=city] input").setValue("Москва");
 
-        Calendar c = Calendar.getInstance(); // текущая дата
-        c.add(Calendar.DATE, 5); // прибавление 5 дней к текущей дате
-        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy"); // формат представления даты
+        String planningDate = generateDate(4); // дата +4 дня от текущей
         form.$("[data-test-id=date] input").sendKeys(Keys.CONTROL + "a" + Keys.BACK_SPACE); // очистка поля даты
-        form.$("[data-test-id=date] input").setValue(formatForDateNow.format(c.getTime()));
+        form.$("[data-test-id=date] input").setValue(planningDate);
 
         form.$("[data-test-id=name] input").setValue("Васильев Петр");
         form.$("[data-test-id=phone] input").setValue("+79811567894");
         form.$$("button").last().click();
 
-        boolean b = $("[data-test-id=agreement].input_invalid").isDisplayed(); // проверка наличия класса input_invalid
-        Assertions.assertTrue(b);
+        $("[data-test-id=agreement].input_invalid").shouldBe(visible); // проверка наличия класса input_invalid
     }
 }
